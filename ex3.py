a6a14471af4d546a9606955543ab6cd91546ab50
ViniciusSito <itoviniciusshigueyuki@gmail.com>
2023-09-10 00:09:59 -0300

    Update ex3.py

--- ex3.py
+++ ex3.py
@@ -21,6 +21,7 @@ def mostrar_palavra(palavra, letras_corretas):
 def jogar_forca():
     palavra = escolher_palavra().upper()
     letras_corretas = []
+    letras_usadas = [] 
     tentativas = 6
 
     print("Bem-vindo ao jogo da Forca!")
@@ -29,18 +30,21 @@ def jogar_forca():
     while tentativas > 0:
         print("\nPalavra: " + mostrar_palavra(palavra, letras_corretas))
         print("Tentativas restantes:", tentativas)
+        print("Letras usadas:", ", ".join(letras_usadas))  
         letra = input("Digite uma letra: ").upper()
         
-        if letra in letras_corretas:
+        if letra in letras_usadas:
             print("Você já tentou essa letra.")
-        elif letra in palavra:
-            letras_corretas.append(letra)
-            if len(set(letras_corretas)) == len(set(palavra)):
-                print("Você venceu! A palavra era:", palavra)
-                break
         else:
-            tentativas -= 1
-            print("Letra incorreta. Tente novamente.")
+            letras_usadas.append(letra) 
+            if letra in palavra:
+                letras_corretas.append(letra)
+                if len(set(letras_corretas)) == len(set(palavra)):
+                    print("Você venceu! A palavra era:", palavra)
+                    break
+            else:
+                tentativas -= 1
+                print("Letra incorreta. Tente novamente.")
 
     if tentativas == 0:
         print("Você perdeu! A palavra era:", palavra)
